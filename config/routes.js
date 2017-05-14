2c3a0712dee01099a2795de927e34d4bfa4057e8
Mark Sherrington <thewayforward3000@gmail.com>
2017-05-14 09:06:42 +0100

    built out routes

---
+++ config/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router  = express.Router();
+
+const statics = require('../controllers/statics');
+
+router.route('/')
+  .get(statics.index);
+
+router.route('*')
+  .get(statics.notFound);
+
+module.exports = router;
