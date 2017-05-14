ecfc7c99b9254151d33cca149726828b5c729a54
Mark Sherrington <thewayforward3000@gmail.com>
2017-05-14 20:09:40 +0100

    added meetapp links

---
+++ public/js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+$(function () {
+  var $header = $('header');
+  var $window = $(window);
+  var $links = $('nav a');
+  var $arrow = $('#arrow');
+  var $projects = $('#projects-link');
+  var $menu = $('.menu');
+
+  $window.scroll(updateHeader).trigger('scroll');
+  $links.on('click', scrollToSection);
+  $arrow.on('click', scrollToSection);
+  $projects.on('click', scrollToSection);
+  $menu.on('click', toggleMenu);
+
+  function toggleMenu() {
+    $('.dropdown').slideToggle();
+  }
+
+  function updateHeader() {
+    var bottomOfHeader = $header.offset().top + $header.height();
+    var viewportHeight = $window.height();
+
+    if (bottomOfHeader >= viewportHeight) {
+      $header.addClass('opaque');
+    } else {
+      $header.removeClass('opaque');
+    }
+  }
+
+  function scrollToSection() {
+    var section = $(this).attr('href');
+    $('body').animate({
+      scrollTop: $(section).offset().top
+    }, 1000, function () {
+      if ($window.width() < 575) {
+        $('.dropdown').slideToggle();
+      }
+    });
+  }
+});
