def17c4e87d317b10dd419b36e07d5debbd2a0fc
Mark Sherrington <thewayforward3000@gmail.com>
2017-05-15 09:22:04 +0100

    added express layouts to server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
 const router = require('./config/routes');
@@ -6,6 +7,7 @@ const router = require('./config/routes');
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 
+app.use(expressLayouts);
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/', router);
